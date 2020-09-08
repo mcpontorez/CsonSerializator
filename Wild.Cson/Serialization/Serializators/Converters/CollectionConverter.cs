ba8a9e98943493e3fa57e3339e3aba3d8c517005
mcpontorez <mcpontorez@hotmail.com>
2020-09-08 16:06:05 +0300

    some refactoring

--- Wild.Cson/Serialization/Serializators/Converters/CollectionConverter.cs
+++ Wild.Cson/Serialization/Serializators/Converters/CollectionConverter.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
 using Wild.Cson.Serialization.Utils;
 
 namespace Wild.Cson.Serialization.Serializators.Converters
